839882f75f965145e31c38c21d60f02ae2e1c611
Steven Sussman <sjsussman@gmail.com>
2021-01-26 14:41:32 -0500

    added base pages

---
+++ src/layouts/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import { TransitionProvider, TransitionViews } from "gatsby-plugin-transitions"
+
+const Layout = ({ location, children }) => {
+  return (
+    <TransitionProvider location={location}>
+      <TransitionViews>{children}</TransitionViews>
+    </TransitionProvider>
+  )
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Steven Sussman's Portfolio</div>
+  return (
+    <div>
+      <h1>Steven Sussman's Portfolio</h1>
+    </div>
+  )
 }
