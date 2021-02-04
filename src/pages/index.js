1c064dbf4ba98289004d162d56f5cbb8ca374d15
Steven Sussman <sjsussman@gmail.com>
2021-02-04 12:45:47 -0500

    added intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,13 @@ import React from "react"
 export default function Home() {
   return (
     <div>
-      <h1>Steven Sussman</h1>
-      <p>Full Stack Web Developer</p>
+      <h1>Hello, my name is Steven Sussman</h1>
+      <p>
+        I am a full stack web developer with an enthusiasm for UX/UI design.
+      </p>
+      <p>Check out my work here.</p>
+      <p>Want to chat? You can reach me here.</p>
+      <p>While you're here, take a look at my tech stack!</p>
     </div>
   )
 }
